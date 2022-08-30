41899ea1448e784b368c331b522dd340bb3e5d3b
Boriwan <boris.bocek@educanet.com>
2022-08-30 12:47:11 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -7,15 +7,26 @@ function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
 
+  //load saved todos upon refresh
   useEffect(() => {
     const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
     if (storedTodos) setTodos(prevTodos => [...prevTodos, ...storedTodos])
   }, [])
 
+  //save todos
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
   }, [todos])
 
+  //toggle todos
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  //add new todos
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
     var id = "id" + Math.random().toString(16).slice(2)
@@ -26,14 +37,19 @@ function App() {
     todoNameRef.current.value = null
   }
 
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
   return (
     <>
       <h1>My React Native To-Do List⚛</h1>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add+</button>
-      <button>Finished</button>
-      <div>0 left to do</div>
+      <button onClick={handleClearTodos}>Clear Complete</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
     </>
   )
 }
